98ec48e3b4294ed0fa580cd732b223fbcd4ecbde
Peter Steinberg <bklynpeter@gmail.com>
2025-10-24 12:00:37 -0400

    tweak how properties distributed in HeaderSection

--- src/components/HeaderSection.tsx
+++ src/components/HeaderSection.tsx
@@ -21,14 +21,16 @@ export default function Header() {
           objectFit='cover'
           objectPosition='50% 20%'
           style={{ filter: 'brightness(73%) contrast(180%) saturate(140%)' }}
-          priority={true} // This ensures the image is loaded with high priority
+          priority={true}
         />
       </div>
 
       {/* Centered logo below the image bar, overlapping with Tailwind */}
       <div
-        className='absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 z-10 bg-white rounded-2xl overflow-hidden flex justify-center items-center
-        w-64 xs:w-80 sm:w-96 md:w-[28rem] lg:w-[32rem]'
+        className='absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 z-10 
+        bg-white rounded-2xl overflow-hidden flex justify-center items-center
+          w-64 xs:w-80 sm:w-96 md:w-[28rem] lg:w-[32rem]
+        '
       >
         <CCLogo />
       </div>
@@ -44,6 +46,7 @@ function CCLogo() {
       src='/assets/CoastalLogoEdited.fig.svg'
       alt='Coastal Concierge Logo'
       className='object-center'
+      objectFit='contain'
       priority
     />
   );
